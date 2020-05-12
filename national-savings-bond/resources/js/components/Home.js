aac0f8b9fc03119ca44db61f9f819969e4819726
Irtaza Alam <irtazaalam786@gmail.com>
2020-05-12 21:07:24 +0500

    css files.

---
+++ national-savings-bond/resources/js/components/Home.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import ReactDOM from 'react-dom';
+
+
+export default function class Example extends Component {
+	render()
+	{
+		return (
+          
+        );
+	}
+}
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Example />, document.getElementById('example'));
+}
